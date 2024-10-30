9efb7c1057d1ae92bf57ae11846eb17101946563
imrahulrai <imrai2448@gmail.com>
2024-10-30 07:31:15 +0530

    content update

--- src/app/website/home/home.component.ts
+++ src/app/website/home/home.component.ts
@@ -10,24 +10,16 @@ export class HomeComponent implements OnInit {
   openItemIndex: number | null = null;
   faqItems = [
     {
-      question: 'How much does Coda AI cost?',
-      answer: 'Unlike other tools where AI is a paid add-on, Coda AI is free for Doc Makers. Editors also receive a free trial of Coda AI. If you’d like to learn more about pricing and usage, visit <a href="#">this help article</a>.'
+      question: 'Does Vegus have NGN Questions?',
+      answer: 'Yes! Vegus has over 1000 next generation NCLEX questions available to ALL members of our NCLEX prep course.'
     },
     {
-      question: 'What models does Coda AI leverage?',
-      answer: 'Coda AI leverages various models to optimize performance, designed for real-time document processing.'
+      question: 'How long does it take to view all NCLEX Review Lecture Series videos?',
+      answer: 'We recommend watching no more than 3 hours of content per day while also taking out time for taking 50 practice questions. While you listen to video u can also take advantage of the note provided in the course.'
     },
     {
-      question: 'How does Coda AI use my data?',
-      answer: 'Coda AI ensures data privacy and security, following industry-standard protocols.'
-    },
-    {
-      question: 'How can I learn more about using Coda AI for work?',
-      answer: 'Visit our tutorials section to learn more about using Coda AI in your projects.'
-    },
-    {
-      question: 'Was there a Coda AI Beta?',
-      answer: 'Yes, Coda AI was available as a beta version to select users before the official release.'
+      question: 'How many months should you prepare for NCLEX?How does Coda AI use my data?',
+      answer: 'The standard average time for preparation is 3 to 4 months, it also depends on various factors like your time spent, focus, retention and smart work.'
     }
   ];
 
